4da78726d2ee69af434ddffd21ad61d05128e52e
Eli Saado <eli5saado@gmail.com>
2021-06-24 01:49:22 +0200

    Add hitter and make it move with arrow keys

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
+const hitterWidth = 100; // hitter is the thing you use to hit the ball
+const hitterHeight = 20; // hitter is the thing you use to hit the ball
+const bottomMargin = 50; // margin between the bottom of the sketch and the hitter
+
+xpos = 0; // 0 is at the middle
+speed = 7;
+
 function draw() {
-  background(220);
+  background(0);
+  if (keyIsDown(LEFT_ARROW)) xpos -= speed;
+  if (keyIsDown(RIGHT_ARROW)) xpos += speed;
+  const hitter = rect(
+    (width - hitterWidth) / 2 + xpos,
+    height - bottomMargin,
+    hitterWidth,
+    hitterHeight
+  );
 }
