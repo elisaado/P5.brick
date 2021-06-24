37858df37b336885c74f252a69fadb3b3caeb145
Eli Saado <eli5saado@gmail.com>
2021-06-24 20:14:32 +0200

    make hitter position mutable

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const hitterWidth = 100; // hitter is the thing you use to hit the ball
 const hitterHeight = 20; // hitter is the thing you use to hit the ball
 const bottomMargin = 50; // margin between the bottom of the sketch and the hitter
 
-const hitter_xpos = 0; // 0 is at the middle
+let hitter_xpos = 0; // 0 is at the middle
 const speed = 7;
 const gravity = 9.81;
 
