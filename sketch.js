41469cdee59a7d1c16b351953368a562fb3470a0
Eli Saado <eli5saado@gmail.com>
2021-06-24 20:12:26 +0200

    Make constant names clearer

--- sketch.js
+++ sketch.js
@@ -6,16 +6,19 @@ const hitterWidth = 100; // hitter is the thing you use to hit the ball
 const hitterHeight = 20; // hitter is the thing you use to hit the ball
 const bottomMargin = 50; // margin between the bottom of the sketch and the hitter
 
-xpos = 0; // 0 is at the middle
-speed = 7;
+const hitter_xpos = 0; // 0 is at the middle
+const speed = 7;
+const gravity = 9.81;
 
 function draw() {
   background(0);
-  xpos = mouseX - width / 2;
+  hitter_xpos = mouseX - width / 2;
   const hitter = rect(
-    (width - hitterWidth) / 2 + xpos,
+    (width - hitterWidth) / 2 + hitter_xpos,
     height - bottomMargin,
     hitterWidth,
     hitterHeight
   );
+
+  const ball = circle();
 }
