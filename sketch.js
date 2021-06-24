de0d02e84266feba81b9abff226757d8b9a833e0
Eli Saado <eli5saado@gmail.com>
2021-06-24 20:17:57 +0200

    use camelCase

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(800, 600);
-  ball_position = [width / 2, height - bottomBallMargin];
+  ballPosition = [width / 2, height - bottomBallMargin];
 }
 
 const hitterWidth = 100; // hitter is the thing you use to hit the ball
@@ -9,20 +9,20 @@ const bottomMargin = 50; // margin between the bottom of the sketch and the hitt
 const bottomBallMargin = 200; // margin between bottom of sketch and initial position of the ball
 const ballDiameter = 50;
 
-let hitter_xpos = 0; // 0 is at the middle
+let hitterXpos = 0; // 0 is at the middle
 const speed = 7;
 const gravity = 9.81;
-let ball_position;
+let ballPosition;
 
 function draw() {
   background(0);
-  hitter_xpos = mouseX - width / 2;
+  hitterXpos = mouseX - width / 2;
   const hitter = rect(
-    (width - hitterWidth) / 2 + hitter_xpos,
+    (width - hitterWidth) / 2 + hitterXpos,
     height - bottomMargin,
     hitterWidth,
     hitterHeight
   );
 
-  const ball = circle(ball_position[0], ball_position[1], ballDiameter);
+  const ball = circle(ballPosition[0], ballPosition[1], ballDiameter);
 }
