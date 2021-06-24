b879d23e1ea3edeb8ee42e4d73a721c50d5d4bb5
Eli Saado <eli5saado@gmail.com>
2021-06-24 21:13:18 +0200

    Add hitboxes and calculateHitbox function

--- sketch.js
+++ sketch.js
@@ -17,6 +17,13 @@ function setup() {
       y: gravity,
     },
 
+    hitbox: {
+      x1: 0,
+      x2: 0,
+      y1: 0,
+      y2: 0,
+    },
+
     diameter: 50,
   };
 
@@ -28,6 +35,13 @@ function setup() {
 
     width: 100,
     height: 20,
+
+    hitbox: {
+      x1: 0,
+      x2: 0,
+      y1: 0,
+      y2: 0,
+    },
   };
 
   hitter.pos.x = (width - hitter.width) / 2;
@@ -36,18 +50,47 @@ function setup() {
 const bottomHitterMargin = 50; // margin between the bottom of the sketch and the hitter
 const bottomBallMargin = 200; // margin between bottom of sketch and initial position of the ball
 
-const gravity = 1;
+const gravity = 0.1;
 
 let ball;
 let hitter;
 
 function draw() {
   background(0);
+  fill(255, 0, 0);
+  calculateHitbox(ball);
+  rect(
+    ball.hitbox.x1,
+    ball.hitbox.y1,
+    ball.hitbox.x2 - ball.hitbox.x1,
+    ball.hitbox.y2 - ball.hitbox.y1
+  );
+  calculateHitbox(hitter);
+
+  fill(255);
   if (mouseX > 0) hitter.pos.x = mouseX - hitter.width / 2;
   rect(hitter.pos.x, hitter.pos.y, hitter.width, hitter.height); // hitter
-
   circle(ball.pos.x, ball.pos.y, ball.diameter); // ball
+
   applyAccelerationAndSpeed(ball);
+}
+
+function calculateHitbox(gameObject) {
+  hb = gameObject.hitbox;
+  if (!hb) return;
+  if (gameObject.diameter) {
+    // it's a ball!
+    hb.x1 = gameObject.pos.x - gameObject.diameter / 2;
+    hb.x2 = gameObject.pos.x + gameObject.diameter / 2;
+    hb.y1 = gameObject.pos.y - gameObject.diameter / 2;
+    hb.y2 = gameObject.pos.y + gameObject.diameter / 2;
+    return;
+  }
+
+  hb.x1 = gameObject.pos.x;
+  hb.x2 = gameObject.pos.x + gameObject.width;
+  hb.y1 = gameObject.pos.y;
+  hb.y2 = gameObject.pos.y + gameObject.height;
 }
 
 function applyAccelerationAndSpeed(gameObject) {
